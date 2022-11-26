014dae620c10f6009fcccff7a2d69722fb4817ec
uv-07 <63532070+uv-07@users.noreply.github.com>
2022-11-27 01:23:32 +0530

    last gugh

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import Card from "./components/card";
+import data from "./components/data";
+// import feed from "../src/img/katie.png";
 
 export default function App() {
+
+  const cards = data.map(item => {
+    return (
+        <Card 
+            // img={feed}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+        />
+    )
+})        
+
   return (
     <React.StrictMode>
       <div>
         <Navbar />
-        {/* <Hero /> */}
-        <Card />
+        <Hero />
+        {cards}
       </div>
     </React.StrictMode>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -1,19 +1,21 @@
 import React from "react";
-import feed from "../img/katie.png";
 import star from "../img/star.png";
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props);
   return (
     <div className="card">
-      <img src={feed} alt="" className="card--image" />
+      <img src={props.img} alt="" className="card--image" />
       <div className="card--stats">
-                <img src={star} alt="" className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
-            </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136 </span>/ person</p>
-        </div>
+        <img src={star} alt="" className="card--star" />
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount})</span>
+        <span className="gray">{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p>
+        <span className="bold">From $ {props.price} </span>/ person
+      </p>
+    </div>
   );
 }
