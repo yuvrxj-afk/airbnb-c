6ea92d526df319809f015a5875bf00a1c8af0503
uv-07 <63532070+uv-07@users.noreply.github.com>
2022-11-26 18:31:29 +0530

    Prop in a while

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import Card from "./components/card";
 
-
 export default function App() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Card />
-    </div>
+    <React.StrictMode>
+      <div>
+        <Navbar />
+        {/* <Hero /> */}
+        <Card />
+      </div>
+    </React.StrictMode>
   );
 }
